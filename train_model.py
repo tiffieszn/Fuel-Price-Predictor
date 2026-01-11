898b816ef5be58207b7bee9f548c2d5ac962f2f9
tiffieszn <tiffanny.dewi13@gmail.com>
2026-01-11 23:26:54 +0700

    update training pipeline to gradient boosting regressor

--- train_model.py
+++ train_model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import joblib
@@ -19,7 +20,7 @@ x = df[features]
 y = df[target]
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-model = RandomForestRegressor(n_estimators=200, random_state=42)
+model = GradientBoostingRegressor(loss='squared_error', learning_rate=0.05, n_estimators=500, max_depth=5, random_state=42, max_features=None)
 model.fit(x_train, y_train)
 
 joblib.dump({"model": model, "encoders": encoders}, "model.pkl")
